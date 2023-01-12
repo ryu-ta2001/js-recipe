44873e7267a093fba0b67f04be15d03b0858b0b1
ryu-ta2001 <aoyagi.ryuta.v8@elms.hokudai.ac.jp>
2023-01-12 18:45:29 +0900

    [JavaScriptのオブジェクト]を学習しました

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -14,7 +14,25 @@ const hikakin = {
 
 // 練習問題
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
 
 // [やってみよう]
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
+const ryuta = {
+  name: "青栁龍太",
+  age: 21,
+  hobbies: ["サッカー観戦", "筋トレ", "運動", "ドライブ"],
+  isHappy: true,
+  address: {
+    country: "日本",
+    city: "北海道",
+  },
+  getName: function () {
+    console.log(this.name)
+  },
+  getAge: function () {
+    console.log(this.age)
+  },
+}
+ryuta.getName()
